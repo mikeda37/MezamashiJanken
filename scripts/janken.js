0c6142b328a2d37d203f7c0ff37e5573a8de27d7
Masahiro Ikeda <mikeda37@gmail.com>
2020-08-06 03:53:04 +0900

    ユーザー情報の変数名、オブジェクト名を変更

--- scripts/janken.js
+++ scripts/janken.js
@@ -3,7 +3,7 @@
  *******************************/
 const url = 'https://www.fujitv.co.jp/meza/d/index.html';
 const puppeteer = require('puppeteer');
-const members = require('./members.json');
+const USERS = require('./users.json');
 
 /**
  * main
@@ -35,16 +35,17 @@ const main = async (week, keyword) => {
     
     const formPage = await newPagePromise;
 
-    for (member of members) {
+    for (user of USERS) {
+
         await formPage.$eval('input[name="DATA1"]', (el, val) => {el.value = val}, keyword);
-        await formPage.$eval('input[name="KJNAME1"]', (el, val) => {el.value = val}, member.name);
-        await formPage.$eval('input[name="KNNAME1"]', (el, val) => {el.value = val}, member.kana);
-        await formPage.select('select[name="AGE"]', member.age);
-        await formPage.click('input[name="GENDER"][value="' + member.gender + '"]');
-        await formPage.$eval('input[name="TEL1"]', (el, val) => {el.value = val}, member.tel);
-        await formPage.$eval('input[name="ZIP"]', (el, val) => {el.value = val}, member.zip);
-        await formPage.select('select[name="PREF"]', member.pref);
-        await formPage.$eval('input[name="ADDR1"]', (el, val) => {el.value = val}, member.address);
+        await formPage.$eval('input[name="KJNAME1"]', (el, val) => {el.value = val}, user.name);
+        await formPage.$eval('input[name="KNNAME1"]', (el, val) => {el.value = val}, user.kana);
+        await formPage.select('select[name="AGE"]', user.age);
+        await formPage.click('input[name="GENDER"][value="' + user.gender + '"]');
+        await formPage.$eval('input[name="TEL1"]', (el, val) => {el.value = val}, user.tel);
+        await formPage.$eval('input[name="ZIP"]', (el, val) => {el.value = val}, user.zip);
+        await formPage.select('select[name="PREF"]', user.pref);
+        await formPage.$eval('input[name="ADDR1"]', (el, val) => {el.value = val}, user.address);
 
         // await formPage.click('input[type="submit"]', {waitUntil: "domcontentloaded"});
         // await formPage.click('input[type="submit"]', {waitUntil: "domcontentloaded"});
